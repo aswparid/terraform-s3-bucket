39488c157c13b2fbec36f2fed255fab35911fb3d
Aswini Parida <aswiniparida@Aswinis-MacBook-Ascendion.local>
2025-08-05 16:43:01 +0100

    feat: update s3 bucket configuration

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -5,11 +5,6 @@ provider "aws" {
 resource "aws_s3_bucket" "tf_state" {
   bucket        = var.bucket_name
   force_destroy = true
-
-  versioning {
-    enabled = true
-  }
-
   lifecycle {
     prevent_destroy = false
   }
@@ -20,12 +15,20 @@ resource "aws_s3_bucket" "tf_state" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "block" {
   bucket = aws_s3_bucket.tf_state.id
 
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
