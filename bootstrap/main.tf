0f7fb91a6a8db59e2c6856c90a3a21d36cb24b09
Aswini Parida <aswiniparida@Aswinis-MacBook-Ascendion.local>
2025-08-07 13:03:49 +0100

    feat: create s3 bucket

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -23,6 +23,16 @@ resource "aws_s3_bucket_versioning" "tf_state_versioning" {
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "block" {
   bucket = aws_s3_bucket.tf_state.id
 
